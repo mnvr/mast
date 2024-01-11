9e02c25fdeb9772a5042f2227cae601da55b5a79
Manav <manav@mrmr.io>
2024-01-11 18:18:22 +0530

    Scaffold a plate to test

--- public/prefix.js
+++ public/prefix.js
@@ -1,5 +1,5 @@
 function generatePrefix({ baseColor }) {
-    return `/* Paste this as the Custom CSS in Server settings > Appearance */
+    return `/* Add this to Server settings > Appearance > Custom CSS */
 
 :root {
     --mast-x1: ${baseColor ?? "red"};
